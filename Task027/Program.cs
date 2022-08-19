19605f3833d05b4179a65ff83cd1671cb9b3f3b8
Dmitry <orionvega5@gmail.com>
2022-08-20 00:09:19 +0300

    Add Task027

---
+++ Task027/Program.cs
@@ -0,0 +1,18 @@
+﻿//  Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+void DigitalSum(int num)
+{
+    int rezult = 0;
+    while (num > 0)
+    {
+        rezult = rezult + (num % 10);
+        num = num / 10;
+    }
+    Console.WriteLine($"Сумма чмсел числа  = {rezult}.");
+}
+
+Console.WriteLine("Введите любое целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+DigitalSum(number);
